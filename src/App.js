8f8569371a1c46e0d69e2874a65404169584f615
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-04-01 21:36:27 -0500

    got About to show up but need to style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Home from './container/Home'
+import About from './container/About';
+import Navigation from './container/Navigation';
+import { Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <header>
+        <Navigation />
+      </header>
+
+        <Routes>
+
+          <Route path='About' element={<About />}/>
+          <Route path='/' element={<Home />}/>
+
+        </Routes>
+
     </div>
   );
 }
